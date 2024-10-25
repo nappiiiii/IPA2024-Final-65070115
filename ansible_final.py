9303871c80577c180a166eafe16caf3b008c7884
apasornn <65070115@kmitl.ac.th>
2024-10-25 15:34:45 +0700

    error ansible

--- ansible_final.py
+++ ansible_final.py
@@ -3,16 +3,11 @@ student_id = "65070115"
 router_name = "CSR1KV-Pod1-2"
 def showrun():
     # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['ansible-playbook', 'ansible/playbook.yml']
+    command = ['ansible-playbook', '-i','ansible/inventory', 'ansible/playbook.yml']
     result = subprocess.run(command, capture_output=True, text=True)
     result = result.stdout
     if 'ok=2' in result:
-        filename = f"show_run_{student_id}_{router_name}.txt"
-
-        
-        with open(filename, 'w') as file:
-            file.write(result)  
-
-        return filename
+        return "ok"
     else:
+        print(result)
         return False
